eb3de22dd1dc401e8225a4e270b1dc99d5b321ee
thomas <thomaspiet@outlook.fr>
2025-03-20 11:47:10 +0100

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Thomas PIET on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
